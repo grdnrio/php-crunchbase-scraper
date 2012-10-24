13a10651a0d0b742ef94b1b588837f618fdc76a1
Joe Gardiner <joe@joegardiner.co.uk>
2012-10-24 14:22:06 +0100

    Added create table for each scrape. Using individual tables for ease of export from phpMyAdmin. No query involved using tables in this way.

---
+++ createTable.php
@@ -0,0 +1,14 @@
+<?php
+
+include'dbConnect.php';
+
+//select table from category being crawled
+mysql_select_db($db_name, $con);
+$sql = "CREATE TABLE IF NOT EXISTS `$category` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `CompanyName` varchar(255) NOT NULL,
+  `Twitter` varchar(255) NOT NULL,
+  `CBURL` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Twitter` (`Twitter`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
