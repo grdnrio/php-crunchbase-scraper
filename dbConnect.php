d2c1d641816772e7e45673093e69001874246b5b
Joe Gardiner <joe@joegardiner.co.uk>
2012-10-31 11:50:37 +0000

    Edited includes for remote testing.

--- dbConnect.php
+++ dbConnect.php
@@ -1,12 +1,14 @@
 <?php
 
-$host = "localhost";
+$host = "jg1.lab.mch.catn.com";
 $db_name = "spider";
 $username = "spidersql";
-$password = "eLYjeA2jDn5BCsdX";
+$password = "TN5TnjZCrK42q7Dd";
 
 $con = mysql_connect("$host","$username","$password");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
+  } else {
+	  var_dump($con);
   }
